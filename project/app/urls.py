3d91e766e9683f96734e333cdca07ffef552c3d2
Sébastien CHAZALLET <pythoniste@protonmail.com>
2024-07-11 12:54:52 +0200

    List view

--- project/app/urls.py
+++ project/app/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-from app.views import HomeView
+from .views import (
+    GameListView,
+)
 
+app_name = "game"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
-    path('game/', include("app.urls")),
-    path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path('', GameListView.as_view(), name="list"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+)
 from django.utils.translation import gettext_lazy as gettext
 
 from .models import Player, Game
-
+from .enums import GameStatus
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -13,4 +16,17 @@ class HomeView(TemplateView):
         data['nb_players'] = Player.objects.count()
         data['best_players'] = Player.objects.order_by("-score")[:5]
         data['nb_games'] = Game.objects.count()
+        return data
+
+
+class GameListView(ListView):
+    model = Game
+
+    # def get_queryset(self):
+    #     # return super().get_queryset().filter(status=GameStatus.ONGOING)
+    #     return Game.objects.playable()
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['page_title'] = gettext("Player's list")
         return data
